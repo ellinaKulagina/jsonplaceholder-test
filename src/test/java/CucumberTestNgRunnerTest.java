d4ae371aaf47013707f7dc45af47f563e24609f8
ellinaKulagina <ellina.kulagina@gmail.com>
2023-03-06 16:55:37 +0000

    running tests in parallel

--- src/test/java/CucumberTestNgRunnerTest.java
+++ src/test/java/CucumberTestNgRunnerTest.java
@@ -1,17 +1,10 @@
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-    features = {"src/test/resources/features/"},
-    glue = {"stepdefinitions"},
-    plugin = {"io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"}
+        features = {"src/test/resources/features/"},
+        glue = {"stepdefinitions"},
+        plugin = {"io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"}
 )
 public class CucumberTestNgRunnerTest extends AbstractTestNGCucumberTests {
-
-    @Override
-    @DataProvider(parallel = true)
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
 }
